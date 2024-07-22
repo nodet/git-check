1389549fde67b7591faf341a65a9edb1e18ca20b
nodet <xavier.nodet@gmail.com>
2024-07-22 08:39:06 +0200

    [#5] Filter branches to consider

---
+++ tests/gh-5.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+
+mkdir test-gh-5
+cd test-gh-5
+
+mkdir orig
+cd orig
+git init --initial-branch=main
+git commit --allow-empty -m "Initial"
+git checkout -b branch
+
+cd ..
+git clone orig wc
+cd wc
+
+git config --local check.branches main
+git check
+git check | grep "the tip of origin/main" || exit 1
+
+git config --local check.branches branch
+git check
+git check | grep "the tip of origin/branch" || exit 1
+
+exit 0
